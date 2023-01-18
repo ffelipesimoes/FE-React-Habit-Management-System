3048149d38a9157283e8d91439d4ff21add1ebbe
Felipe Simões <ffelipesimoes@gmail.com>
2023-01-18 20:02:02 -0300

    feat: install vite/react

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./styles/global.css"
+import { Habit } from './components/Habit'
+
+
+function App() {
+
+  return (
+    <div>
+     
+      <Habit completed={2}/>
+      <Habit completed={2}/>
+      <Habit completed={2}/>
+
+    </div>
+  )
+}
+
+export default App
