2cfff8f02eae3b6b393f35eb64e77318409d4bfd
Felipe Simões <ffelipesimoes@gmail.com>
2023-01-22 14:59:08 -0300

    feat: create summary habit table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./styles/global.css"
 // import { Habit } from './components/Habit'
 import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
 export function App() {
 
@@ -8,7 +9,8 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         
-       <Header/>
+       <Header />
+       <SummaryTable />
 
       </div>
     </div>
---
+++ src/components/HabitDay.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// interface HabitDayProps {
+//   completed: number
+// }
+
+export const HabitDay = () => {
+  return (
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"> </div>
+  )
+}
